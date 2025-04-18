d9e04040ef36922cc5cd234c168b58416f3e7817
Zdeněk Sojma <zdenek@sojma.cz>
2025-04-18 14:05:52 +0200

    Update Electrum-JSONRPC submodule to use zsojma's fork

--- Response/AbstractResponseClass.cs
+++ Response/AbstractResponseClass.cs
@@ -1,13 +1,14 @@
 ﻿////////////////////////////////////////////////
 // © https://github.com/badhitman - @fakegov 
 ////////////////////////////////////////////////
-using MultiTool;
+using System;
+using System.Globalization;
 using System.Runtime.Serialization;
 
 namespace ElectrumJSONRPC.Response
 {
     [DataContract]
-    public abstract class AbstractResponseClass : exClasses.JsonSerClass
+    public abstract class AbstractResponseClass
     {
         [DataMember]
         public string id { get; set; }
@@ -15,12 +16,27 @@ namespace ElectrumJSONRPC.Response
         [DataMember]
         public ErrorResponseClass error { get; set; }
 
-        public static double BtcStringDoubleValue(string str_value)
+        protected static double GetDoubleValueFromString(string str_value)
         {
+            if (string.IsNullOrEmpty(str_value))
+                return 0;
+
+            str_value = str_value.Trim();
             if (str_value.Length > 5 && str_value.Substring(str_value.Length - 3).ToUpper() == "BTC")
                 str_value = str_value.Substring(0, str_value.Length-3).Trim();
 
-            return glob_tools.GetDoubleFromString(str_value);
+            return GetDoubleFromString(str_value);
+        }
+
+        private static double GetDoubleFromString(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return 0;
+
+            if (double.TryParse(value.Replace(",", "."), NumberStyles.Any, CultureInfo.InvariantCulture, out double result))
+                return result;
+
+            return 0;
         }
     }
 
